67f857d616ef199ec2160e2b639c63f28304be93
llucasdaniel <l.lucasdaniel@hotmail.com>
2019-01-17 23:26:48 -0200

    Add Enums Perfil e TipoEnum

---
+++ src/main/java/com/lucas/pontoeletronico/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.lucas.pontoeletronico.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO
+}
---
+++ src/main/java/com/lucas/pontoeletronico/api/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.lucas.pontoeletronico.api.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+}
