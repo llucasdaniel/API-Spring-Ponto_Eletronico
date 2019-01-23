3c1c1371c7aacd372fa88ce7d9d00d51441d43ae
llucasdaniel <l.lucasdaniel@hotmail.com>
2019-01-22 23:43:54 -0200

    FuncionarioService Interface

---
+++ src/main/java/com/lucas/pontoeletronico/api/services/FuncionarioService.java
@@ -0,0 +1,39 @@
+package com.lucas.pontoeletronico.api.services;
+
+import java.util.Optional;
+
+import com.lucas.pontoeletronico.api.entities.Funcionario;
+
+public interface FuncionarioService {
+
+	/**
+	 * Return Funcionario by CPF
+	 * 
+	 * @param CPF
+	 * @return
+	 */
+	Optional<Funcionario> buscarPorCPF(String cpf);
+	
+	/**
+	 * Return Funcionario by Email
+	 * 
+	 * @param Email
+	 * @return
+	 */
+	Optional<Funcionario> buscarPorEmail(String email);
+	
+	/**
+	 * Return Funcionario by ID
+	 * 
+	 * @param ID
+	 * @return
+	 */
+	Optional<Funcionario> buscarPorId(Long id);
+	
+	/**
+	 * Add Funcionario 
+	 * @param Funcionario
+	 * @return
+	 */
+	Funcionario add(Funcionario funccionario);
+}
