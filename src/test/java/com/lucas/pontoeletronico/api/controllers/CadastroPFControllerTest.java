f05d65332a1ba16bf4c5e3498ec68595533fdbdd
llucasdaniel <l.lucasdaniel@hotmail.com>
2019-01-27 12:32:24 -0200

    more tests for CadastroPFController

--- src/test/java/com/lucas/pontoeletronico/api/controllers/CadastroPFControllerTest.java
+++ src/test/java/com/lucas/pontoeletronico/api/controllers/CadastroPFControllerTest.java
@@ -1,24 +1,36 @@
 package com.lucas.pontoeletronico.api.controllers;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
 
 import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.validation.BindingResult;
 
 import com.lucas.pontoeletronico.api.dtos.CadastroPFDto;
 import com.lucas.pontoeletronico.api.entities.Empresa;
 import com.lucas.pontoeletronico.api.entities.Funcionario;
 //import static org.junit.Assert.;
+import com.lucas.pontoeletronico.api.enums.PerfilEnum;
+import com.lucas.pontoeletronico.api.utils.PasswordUtils;
 
 public class CadastroPFControllerTest {
 
 	private Empresa emp;
 	
 	private Funcionario func;
+	
+	private CadastroPFDto cadastroPFDto;
+	
+	@Mock
+	private CadastroPFController controller;
+	
+	private BindingResult bindResult;
 	
 	@Before
 	public void setUp() {
@@ -34,10 +46,21 @@ public class CadastroPFControllerTest {
 		func.setQtdHorasAlmoco(1F);
 		func.setQtdHorasTrabalhoDia(8F);
 		func.setValorHora(new BigDecimal(60));	
+		
+		cadastroPFDto = new CadastroPFDto();
+		cadastroPFDto.setId(10L);
+		cadastroPFDto.setNome("Lucas D");
+		cadastroPFDto.setEmail("lucas@123.com");
+		cadastroPFDto.setCpf("123456789009");
+		cadastroPFDto.setCnpj(emp.getCnpj());
+		cadastroPFDto.setQtdHorasAlmoco(Optional.of("1"));
+		cadastroPFDto.setQtdHorasTrabalhoDia(Optional.of("8"));
+		cadastroPFDto.setValorHora(Optional.of("60"));	
+		cadastroPFDto.setSenha("123");	
 	}
 	
 	@Test
-	public void converterCadastroPFDtoTest_OptionalPresent() {
+	public void converterCadastroPFDtoTest() {
 		CadastroPFController controller = new  CadastroPFController();
 		CadastroPFDto cad = controller.converterCadastroPFDto(this.func);
 	
@@ -54,12 +77,12 @@ public class CadastroPFControllerTest {
 	
 	@Test
 	public void converterCadastroPFDtoTest_OptionalNOTPresent() {
-		func.setQtdHorasAlmoco(null);
-		func.setQtdHorasTrabalhoDia(null);
-		func.setValorHora(null);
-		
-		CadastroPFController controller = new  CadastroPFController();
-		CadastroPFDto cad = controller.converterCadastroPFDto(this.func);
+		Funcionario funcNotDefault = func;
+		funcNotDefault.setQtdHorasAlmoco(null);
+		funcNotDefault.setQtdHorasTrabalhoDia(null);
+		funcNotDefault.setValorHora(null);
+		CadastroPFController controller1 = new  CadastroPFController();
+		CadastroPFDto cad = controller1.converterCadastroPFDto(funcNotDefault);
 	
 		assertEquals("Lucas D",cad.getNome());
 		assertEquals("lucas@123.com",cad.getEmail());
@@ -70,6 +93,27 @@ public class CadastroPFControllerTest {
 		assertEquals(Optional.empty(),cad.getQtdHorasAlmoco());
 		assertEquals(Optional.empty(),cad.getQtdHorasTrabalhoDia());
 		assertEquals(Optional.empty(),cad.getValorHora());
+	}
+	
+	@Test
+	public void converterDtoParaFuncionarioTest() throws NoSuchAlgorithmException {
+		this.bindResult = mock(BindingResult.class);
+		CadastroPFController controller1 = new  CadastroPFController();
+		Funcionario funcionario = controller1.converterDtoParaFuncionario(cadastroPFDto, this.bindResult);
+		
+		assertEquals("Lucas D",funcionario.getNome());
+		assertEquals("lucas@123.com",funcionario.getEmail());
+		assertEquals("123456789009",funcionario.getCpf());
+		
+		assertEquals(PasswordUtils.generateBCrypt(cadastroPFDto.getSenha()),funcionario.getSenha());
+		assertEquals(PerfilEnum.ROLE_USUARIO,funcionario.getPerfil());
+		
+		
+		
+		assertEquals(new Float(1),funcionario.getQtdHorasAlmoco());
+		assertEquals(new Float(8.0),funcionario.getQtdHorasTrabalhoDia());
+		assertEquals(new BigDecimal(60),funcionario.getValorHora());
+		
 	}
 		
 }
