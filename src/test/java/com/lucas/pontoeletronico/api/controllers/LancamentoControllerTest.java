e49a3aa03619822e4cf3de39d64df5fa9a8686e4
llucasdaniel <l.lucasdaniel@hotmail.com>
2019-02-01 18:11:16 -0200

    MockUser add

--- src/test/java/com/lucas/pontoeletronico/api/controllers/LancamentoControllerTest.java
+++ src/test/java/com/lucas/pontoeletronico/api/controllers/LancamentoControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,6 +35,7 @@ import com.lucas.pontoeletronico.api.services.LancamentoService;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test") 
+@WithMockUser 
 public class LancamentoControllerTest {
 	
 	@Autowired
