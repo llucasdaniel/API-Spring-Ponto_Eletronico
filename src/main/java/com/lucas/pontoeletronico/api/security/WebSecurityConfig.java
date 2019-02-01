3f6bf005701b5e735a53770407632813dd647da6
llucasdaniel <l.lucasdaniel@hotmail.com>
2019-02-01 18:24:15 -0200

    fix error security

--- src/main/java/com/lucas/pontoeletronico/api/security/WebSecurityConfig.java
+++ src/main/java/com/lucas/pontoeletronico/api/security/WebSecurityConfig.java
@@ -1,20 +1,24 @@
-package com.lucas.pontoeletronico.api.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // For example: Use only Http Basic and not form login.
-        http
-            .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-            .httpBasic();
-    }
-}
+//package com.lucas.pontoeletronico.api.security;
+//
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@EnableWebSecurity
+//@Configuration
+//@ConditionalOnProperty(value = "security.basic.enabled", havingValue = "false")
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // For example: Use only Http Basic and not form login.
+//        http
+//            .authorizeRequests()   
+//            .antMatchers( "/api/cadastrar-pj")
+//            .permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//            .httpBasic();
+//    }
+//}
