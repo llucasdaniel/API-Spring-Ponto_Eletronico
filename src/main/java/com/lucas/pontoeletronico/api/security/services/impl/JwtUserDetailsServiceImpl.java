44f814ab5cec436548474671cce7e7465eac06dc
llucasdaniel <l.lucasdaniel@hotmail.com>
2019-02-10 10:44:29 -0200

    Add Classes for JsonWebToken and Spring Security

---
+++ src/main/java/com/lucas/pontoeletronico/api/security/services/impl/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.lucas.pontoeletronico.api.security.services.impl;
+
+import com.lucas.pontoeletronico.api.entities.Funcionario;
+import com.lucas.pontoeletronico.api.security.JwtUserFactory;
+import com.lucas.pontoeletronico.api.services.FuncionarioService;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+        if (funcionario.isPresent()) {
+            return JwtUserFactory.create(funcionario.get());
+        }
+
+        throw new UsernameNotFoundException("Email não encontrado.");
+    }
+
+}
